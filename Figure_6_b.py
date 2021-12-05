043e594a369d4e1f902f24e93b3ab2f9e3ac238e
annafariha <afariha2021@gmail.com>
2021-12-05 14:01:51 -0800

    Update

--- Figure_6_b.py
+++ Figure_6_b.py
@@ -87,95 +87,88 @@ import numpy as np
 import prose.datainsights as di
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
-from scipy.stats.stats import pearsonr   
+from scipy.stats.stats import pearsonr
 
+N = 5000  # number of data points in the train/test set
+n_test_cases = 10  # number of experiments to run
+step = 5
 
-N = 5000              # number of data points in the train/test set
-n_test_cases = 10    # number of experiments to run
-step = 10
+np.random.seed(1)
 
 score_drops = []
 our_violations = []
 
-# cd_area_violations = []
-# cd_mkl_violations = []
-# spll_violations = []
-
 kk = 10
-for kk in range(5, 56, step):
-    mix_fraction = (kk * N)//100      
-    print(kk, mix_fraction)
-    
+for kk in range(0, 51, step):
+    mix_fraction = (kk * N) // 100
+    print("Fraction of mobile data: ", kk, '%')
+
     score_drop_ = 0
     our_violation_ = 0
-    
-    #     cd_area_violation_ = 0
-    #     cd_mkl_violation_ = 0
-    #     spll_violation_ = 0
-
     cs = 0
-    
+
+    train_df, train_X, train_y = get_data_for_activities(mobile_activities, N=N)
+
     for _ in range(n_test_cases):
-        test_df, test_X, test_y = get_data_for_activities(mobile_activities, N=N)
-        train_df_1, train_X_1, train_y_1 = get_data_for_activities(mobile_activities, N = mix_fraction)
-        train_df_2, train_X_2, train_y_2 = get_data_for_activities(sedentary_activities, N = N - mix_fraction)
+        test_df_1, test_X_1, test_y_1 = get_data_for_activities(mobile_activities, N=mix_fraction)
+        test_df_2, test_X_2, test_y_2 = get_data_for_activities(sedentary_activities, N=N - mix_fraction)
+        test_df = pd.concat([test_df_1, test_df_2], axis=0)
+        test_X = np.vstack((test_X_1, test_X_2))
+        test_y = np.concatenate([test_y_1, test_y_2], axis=0)
 
-        train_df = pd.concat([train_df_1, train_df_2], axis=0)
-        train_X = np.vstack((train_X_1, train_X_2))
-        train_y = np.concatenate([train_y_1, train_y_2], axis=0)
-
-        assertions = di.learn_assertions(train_df, max_self_violation=0.85)
+        assertions = di.learn_assertions(pd.DataFrame(test_X), max_self_violation=0.85)
         clf = LogisticRegression(random_state=0, solver='lbfgs', multi_class='multinomial')
-        clf.fit(train_X, train_y)
+        clf.fit(test_X, test_y)
 
-        max_score = clf.score(train_X, train_y)
+        max_score = clf.score(test_X, test_y)
 
-        score = clf.score(test_X, test_y)
+        score = clf.score(train_X, train_y)
         score_drop = max_score - score
-        our_violation = assertions.evaluate(test_df).avg_violation     
-        
+        our_violation = assertions.evaluate(pd.DataFrame(train_X)).avg_violation
+
         score_drop_ += score_drop
         our_violation_ += our_violation
         cs += 1
-        
-        print(cs, end = '')
-    
-    # print('')
-            
+
+        print(cs, end='')
+
+    print('')
+
     score_drop = score_drop_ / cs
-    our_violation = our_violation_ / cs        
-        
-    print("Drift fraction:", mix_fraction/float(N))
+    our_violation = our_violation_ / cs
+
+    print("Drift fraction:", mix_fraction / float(N))
     print("ML score drop:", score_drop)
     print("Violations Our:", our_violation)
-    
+
     score_drops.append(score_drop)
-    our_violations.append(our_violation)   
-    
+    our_violations.append(our_violation)
+
     if len(score_drops) > 1 and cs % 10 == 0:
         print("Done", cs, "tests")
-        print("PCC score-vs-ours", pearsonr(score_drops,our_violations))
+        print("PCC score-vs-ours", pearsonr(score_drops, our_violations))
 
 systemName = 'CCSynth'
 
 print(score_drops)
 print(our_violations)
+
 print("PCC & P-Value", pearsonr(score_drops,our_violations))
     
 fig = plt.gcf()
-fig.set_size_inches(2.4, 2.2)
+fig.set_size_inches(2.4, 4)
 rcParams['font.family'] = 'serif'
 rc('text', usetex=True)
 rcParams['figure.dpi'] = 300
 
-plt.scatter(np.arange(10, 110, 10), our_violations, marker="s",c="C3", label=systemName)
-plt.scatter(np.arange(10, 110, 10), score_drops, marker="o", c="C0", label="Classifier (LR)")
+plt.scatter(np.arange(1, 12, 1) * 5.0, our_violations, "C3s", label=systemName)
+plt.scatter(np.arange(1, 12, 1) * 5.0, score_drops, "C0o", label="Classifier (LR)")
 
-plt.legend(ncol=1, loc='upper left')
+plt.legend(ncol=1, loc='upper right', handletextpad=0.1, handlelength=1)
 plt.ylim([0,1])
-plt.xlabel("Noise (\%) during training", fontsize=14)
+plt.xlabel("Noise (\%) during training")
 plt.ylabel("CC Violation/acc-drop", fontsize=14)
-plt.xticks(np.arange(10, 100, 20))
+plt.xticks(np.arange(1, 12, 2) * 5)
 
 plt.savefig(os.path.join("Plots", "Figure_6_b.pdf"), bbox_inches="tight")
 plt.show()
